85a04a9a5d2ed08d6b879e7b0bd5c40e543ea14e
dwain-barnes <69762991+dwain-barnes@users.noreply.github.com>
2024-06-28 17:19:08 +0100

    Update cyberConcepts.js
    add description for the cards

--- js/cyberConcepts.js
+++ js/cyberConcepts.js
@@ -1,11 +1,42 @@
 const cyberConcepts = [
-    { name: "Firewall", image: "img/firewall.png" },
-    { name: "Encryption", image: "img/encryption.png" },
-    { name: "Phishing", image: "img/phishing.png" },
-    { name: "Malware", image: "img/malware.png" },
-    { name: "VPN", image: "img/vpn.png" },
-    { name: "Two-step Verification", image: "img/2Sv.png" },
-    { name: "Backup", image: "img/backup.png" },
-    { name: "Antivirus", image: "img/antivirus.png" },
-    // Add more concepts as needed
+    {
+        name: "Firewall",
+        image: "img/firewall.png",
+        description: "A digital guard that watches what goes in and out of your computer, keeping the bad stuff out while letting the good stuff through."
+    },
+    {
+        name: "Encryption",
+        image: "img/encryption.png",
+        description: "A way to scramble your information so that only people with a special key can read it, like a secret code for your data."
+    },
+    {
+        name: "Phishing",
+        image: "img/phishing.png",
+        description: "Tricks used by bad guys to fool you into giving away your personal information, like fake emails pretending to be from your bank."
+    },
+    {
+        name: "Malware",
+        image: "img/malware.png",
+        description: "Bad software that can harm your computer or steal your information, like a virus for your digital device."
+    },
+    {
+        name: "VPN",
+        image: "img/vpn.png",
+        description: "A tool that creates a private, secure tunnel for your internet connection, helping to keep your online activities private."
+    },
+    {
+        name: "Two-step Verification",
+        image: "img/2Sv.png",
+        description: "An extra layer of security that asks for a second proof of identity, like a code sent to your phone, to make sure it's really you logging in."
+    },
+    {
+        name: "Backup",
+        image: "img/backup.png",
+        description: "Making extra copies of your important files and storing them somewhere safe, so you don't lose them if something goes wrong with your device."
+    },
+    {
+        name: "Antivirus",
+        image: "img/antivirus.png",
+        description: "A digital guard dog that looks for and removes harmful software from your computer to keep it safe and healthy."
+    }
 ];
